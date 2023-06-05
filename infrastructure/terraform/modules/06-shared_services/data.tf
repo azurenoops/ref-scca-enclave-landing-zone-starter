a2e7fcc6549afdf82e3761a52a92ed76cff59f32
John Spinella <jrspinella@users.noreply.github.com>
2023-06-05 15:46:42 -0400

    Added PE subnet lookup

--- infrastructure/terraform/modules/06-shared_services/data.tf
+++ infrastructure/terraform/modules/06-shared_services/data.tf
@@ -7,6 +7,12 @@ data "azurerm_subnet" "svcs_subnet" {
   resource_group_name  = var.resource_group_name
 }
 
+data "azurerm_subnet" "svcs_pe_subnet" {
+  name                 = "anoa-eus-svcs-core-dev-pe-snet"
+  virtual_network_name = var.virtual_network_name
+  resource_group_name  = var.resource_group_name
+}
+
 data "azurerm_subnet" "svcs_vm_subnet" {
   name                 = "anoa-eus-svcs-core-dev-vm-snet"
   virtual_network_name = var.virtual_network_name
