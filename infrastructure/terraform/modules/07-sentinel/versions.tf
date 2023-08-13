a2c7245a0f6fc017d69c5c9a331be59dab46c2d7
John Spinella <jrspinella@users.noreply.github.com>
2023-08-13 10:09:52 -0400

    Updates to Sentinel

---
+++ infrastructure/terraform/modules/07-sentinel/versions.tf
@@ -0,0 +1,15 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+/*
+SUMMARY: Sentinal Alerts Module
+AUTHOR/S: jspinella, Curtis Slone
+*/
+
+# Azure provider version 
+provider "azurerm" {    
+    features {} 
+}
+
+
+
