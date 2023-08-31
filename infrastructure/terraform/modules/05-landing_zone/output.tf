b4cfc4be62be680d168eb6aad405e921b3ae270e
John Spinella <jrspinella@users.noreply.github.com>
2023-08-31 14:54:51 -0400

    Update outputs

--- infrastructure/terraform/modules/05-landing_zone/output.tf
+++ infrastructure/terraform/modules/05-landing_zone/output.tf
@@ -25,13 +25,18 @@ output "hub_default_subnet_ids" {
 }
 
 output "hub_default_subnet_names" {
-  description = "The name of the default subnet"
+  description = "The names of the default subnets"
   value       = module.mod_hub_network.subnet_names
 }
 
 output "hub_storage_account_id" {
-  description = "The name of the default subnet"
+  description = "The ids of the default subnets"
   value       = module.mod_hub_network.storage_account_id
+}
+
+output "hub_default_nsg_names" {
+  description = "The names of each hub network security groups"
+  value       = module.mod_hub_network.network_security_group_names
 }
 
 # hub_network firewall module outputs
@@ -64,22 +69,27 @@ output "hub_private_dns_zone_ids" {
 # id_network module outputs
 output "identity_resource_group_name" {
   description = "The name of the Identity resource group"
-  value       = module.mod_ops_network.resource_group_name
+  value       = module.mod_id_network.resource_group_name
 }
 
 output "identity_virtual_network_name" {
   description = "The name of the Identity spoke virtual network"
-  value       = module.mod_ops_network.virtual_network_name
+  value       = module.mod_id_network.virtual_network_name
 }
 
 output "identity_default_subnet_ids" {
-  description = "The ids of the Identity default subnet"
-  value       = module.mod_ops_network.subnet_ids
+  description = "The ids of the Identity default subnets"
+  value       = module.mod_id_network.subnet_ids
 }
 
 output "identity_default_subnet_names" {
-  description = "The names of the Identity default subnet"
-  value       = module.mod_ops_network.subnet_ids_names
+  description = "The names of the Identity default subnets"
+  value       = module.mod_id_network.subnet_names
+}
+
+output "identity_default_nsg_names" {
+  description = "The names of each Identity network security groups"
+  value       = module.mod_id_network.network_security_group_names
 }
 
 # ops_network module outputs
@@ -94,13 +104,18 @@ output "operations_virtual_network_name" {
 }
 
 output "operations_default_subnet_ids" {
-  description = "The ids of the Operations default subnet"
+  description = "The ids of the Operations default subnets"
   value       = module.mod_ops_network.subnet_ids
 }
 
 output "operations_default_subnet_names" {
-  description = "The names of the Operations default subnet"
-  value       = module.mod_ops_network.subnet_ids_names
+  description = "The names of the Operations default subnets"
+  value       = module.mod_ops_network.subnet_names
+}
+
+output "operations_default_nsg_names" {
+  description = "The names of each Operations network security groups"
+  value       = module.mod_ops_network.network_security_group_names
 }
 
 # operational logging module outputs
@@ -142,11 +157,16 @@ output "devsecops_virtual_network_name" {
 }
 
 output "devsecops_default_subnet_ids" {
-  description = "The ids of the DevSecOps default subnet"
+  description = "The ids of the DevSecOps default subnets"
   value       = module.mod_devsecops_network.subnet_ids
 }
 
 output "devsecops_default_subnet_names" {
-  description = "The names of the DevSecOps default subnet"
-  value       = module.mod_devsecops_network.subnet_ids_names
+  description = "The names of the DevSecOps default subnets"
+  value       = module.mod_devsecops_network.subnet_names
+}
+
+output "devsecops_default_nsg_names" {
+  description = "The names of each DevSecOps network security groups"
+  value       = module.mod_devsecops_network.network_security_group_names
 }
