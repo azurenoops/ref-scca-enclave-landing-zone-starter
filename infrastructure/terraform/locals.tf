fefad3611f533449395a5f015377842380b2a4ee
John Spinella <jrspinella@users.noreply.github.com>
2023-09-11 11:23:54 -0400

    update locals for telemtry

--- infrastructure/terraform/locals.tf
+++ infrastructure/terraform/locals.tf
@@ -146,3 +146,33 @@ locals {
     after_azurerm_management_group = var.destroy_duration_delay["azurerm_management_group"]
   }
 }
+
+# The following locals identify the module
+locals {
+  # PUID identifies the module
+  telem_management_puid = "34603aac-98f8-4a55-92fc-4c78378c9ba5"
+}
+
+# The following `can()` is used for when disable_telemetry = true
+locals {
+  telem_random_hex = can(random_id.telem[0].hex) ? random_id.telem[0].hex : local.empty_string
+}
+
+# Here we create the ARM templates for the telemetry deployment
+locals {
+  telem_arm_subscription_template_content = <<TEMPLATE
+{
+  "$schema": "https://schema.management.azure.com/schemas/2018-05-01/subscriptionDeploymentTemplate.json#",
+  "contentVersion": "1.0.0.0",
+  "parameters": {},
+  "variables": {},
+  "resources": [],
+  "outputs": {
+    "telemetry": {
+      "type": "String",
+      "value": "For more information, see https://aka.ms/azurenoops/tf/telemetry"
+    }
+  }
+}
+TEMPLATE
+}
