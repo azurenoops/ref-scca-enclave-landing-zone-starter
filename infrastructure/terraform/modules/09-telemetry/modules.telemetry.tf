c6fb9176c3d1431a8d4eef10a5f1ee5f0330c26f
John Spinella <jrspinella@users.noreply.github.com>
2023-09-11 11:22:46 -0400

    move tf-rem-state & telemtry

--- infrastructure/terraform/modules/09-telemetry/modules.telemetry.tf
+++
@@ -1,8 +0,0 @@
-# Copyright (c) Microsoft Corporation.
-# Licensed under the MIT License.
-
-# The following random id is created once per module instantiation and is appended to the teleletry deployment name
-resource "random_id" "telem" {
-  count       = local.disable_telemetry ? 0 : 1
-  byte_length = 4
-}
