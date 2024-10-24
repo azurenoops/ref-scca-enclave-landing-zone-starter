c9d6a4752a4086bc518f6621bf3ac12be859f08a
John Spinella <jrspinella@users.noreply.github.com>
2024-10-23 21:48:09 -0400

    Added changes to Subnet variable

--- infrastructure/terraform/variables.landing.zone.hub.tf
+++ infrastructure/terraform/variables.landing.zone.hub.tf
@@ -34,7 +34,44 @@ variable "ampls_subnet_address_prefixes" {
 
 variable "hub_subnets" {
   description = "The subnets of the hub virtual network."
-  default     = {}
+  type = map(object({
+    #Basic info for the subnet
+    name                                       = string
+    address_prefixes                           = list(string)
+    service_endpoints                          = list(string)
+    private_endpoint_network_policies_enabled  = string
+    private_endpoint_service_endpoints_enabled = bool
+
+    # Delegation block - see https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/subnet#delegation
+    delegation = optional(object({
+      name = string
+      service_delegation = object({
+        name    = string
+        actions = list(string)
+      })
+    }))
+
+    #Subnet NSG rules - see https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/network_security_group#security_rule
+    nsg_subnet_rules = optional(map(object({
+      name                                       = string
+      description                                = string
+      priority                                   = number
+      direction                                  = string
+      access                                     = string
+      protocol                                   = string
+      source_port_range                          = optional(string)
+      source_port_ranges                         = optional(list(string))
+      destination_port_range                     = optional(string)
+      destination_port_ranges                    = optional(list(string))
+      source_address_prefix                      = optional(string)
+      source_address_prefixes                    = optional(list(string))
+      source_application_security_group_ids      = optional(list(string))
+      destination_address_prefix                 = optional(string)
+      destination_address_prefixes               = optional(list(string))
+      destination_application_security_group_ids = optional(list(string))
+    })))
+  }))
+  default = {}
 }
 
 variable "enable_traffic_analytics" {
